16175be95170ae214e0af05c6f65619d9ae0993f
Makram Kamaleddine <makram@500px.com>
2017-06-05 12:06:35 -0400

    Add more git aliases

--- zshrc.zsh
+++ zshrc.zsh
@@ -19,12 +19,17 @@ alias grm="git rm"
 # Commit-related aliases
 alias gcsam="git commit -S -a -m"
 alias gcsav="git commit -vaS"
+alias gcsv="git commit -vS"
 
 alias gs="git show"
 alias gi="git init"
 alias gcob="git checkout -b"
 
 alias gfp="git fetch --prune"
+alias gds="git diff --staged"
+
+# i.e git push this the fuck outta here
+alias gpfoh="git push -f origin head"
 
 alias vim=nvim
 
