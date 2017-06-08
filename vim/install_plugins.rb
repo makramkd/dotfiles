bd150eeb1f3ab79f0234c67b3d146ca22706bc8d
Makram Kamaleddine <makram@500px.com>
2017-06-08 11:26:34 -0400

    Add install_plugins.rb script

---
+++ vim/install_plugins.rb
@@ -0,0 +1,9 @@
+# Installs plugins so that they can be used by pathogen
+# i.e make sure you have pathogen setup: https://github.com/tpope/vim-pathogen
+# Note that this is most likely incorrect for windows
+
+vim_plugins_directory = '~/.vim/bundle'
+remotes = File.open('installed_plugins.txt').read
+remotes.each_line do |line|
+  `git clone #{line} #{vim_plugins_directory}`
+end
