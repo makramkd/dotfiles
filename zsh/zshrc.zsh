7700fc620e0758bb8b0135ca148f846008fada69
Makram Kamaleddine <makram.kd@gmail.com>
2018-03-29 15:38:40 -0400

    Get rid of custom PS1 and aliases

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,5 +1,5 @@
 # Common zsh functionality
-# 
+#
 # This file is meant to be sourced from the .zshrc, typically contained
 # in the home directory.
 #
@@ -7,9 +7,6 @@
 dir="$(dirname "$0")"
 
 autoload -U compinit && compinit
-
-# Set a basic PS1
-PROMPT="Ξ %20<...<%~%<< %# "
 
 # Compinstall basics
 fpath=(/usr/local/share/zsh-completions $fpath)
@@ -34,21 +31,8 @@ export CXX=/usr/bin/clang++
 
 export EDITOR=vim
 
-# Aliases
-alias vim=nvim
-
-# C/C++ aliases
-# Install GCC6 with homebrew on Mac
-alias gcc6=gcc-6
-alias g++6=g++-6
-alias cpp6=c++-6
-
 # Key bindings
 # For a comprehensive guide, see http://zsh.sourceforge.net/Guide/zshguide04.html
-
-# Ensure emacs mode, depsite us being vim developers
-bindkey -e
-
 # Same line navigation
 # NOTE: overrides vanilla C-b and C-f (backward-char and forward-char)
 bindkey '^B' backward-word
@@ -57,29 +41,11 @@ bindkey '^F' forward-word
 # Misc things
 stty -ixon
 
-# History
-HISTSIZE=100000
-
-# syntax highlighting for less and cat
-# do: brew install highlight
-# or: brew install source-highlight
-# uncomment below to use source-highlight
-# export LESSOPEN="| src-hilite-lesspipe.sh %s"
-# This uses highlight
-export LESSOPEN="| $(which highlight) %s --out-format xterm256 --quiet --force --style solarized-dark"
-export LESS=" -R "
-alias less="less -m -N -g -i -J --underline-special --SILENT"
-alias rless="less +G"
-alias more="less"
-
-# Use highlight in place of cat
-alias cat="highlight $1 --out-format xterm256 --quiet --force --style solarized-light"
-
 # Custom aliases and functions
 local -a plugins
 plugins=(rails.zsh git.zsh utility.zsh)
 
-# Don't loop through current directory because we'd recursively 
+# Don't loop through current directory because we'd recursively
 # source this file
 for i in $plugins; do
   source $dir/$i end
