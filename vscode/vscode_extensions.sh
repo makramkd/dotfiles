23f1d1ae625b334c9f24f84a34defdbf579a8ec2
Makram Kamaleddine <makram.kd@gmail.com>
2018-08-16 16:29:17 -0400

    Add vscode configuration

---
+++ vscode/vscode_extensions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+code --install-extension akamud.vscode-theme-onedark
+code --install-extension azemoh.one-monokai
+code --install-extension be5invis.vscode-icontheme-nomo-dark
+code --install-extension cometeer.spacemacs
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+code --install-extension Equinusocio.vsc-material-theme
+code --install-extension IBM.output-colorizer
+code --install-extension johnpapa.winteriscoming
+code --install-extension jq-syntax-highlighting.jq-syntax-highlighting
+code --install-extension mauve.terraform
+code --install-extension mitaki28.vscode-clang
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.Go
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension PKief.material-icon-theme
+code --install-extension quicktype.quicktype
+code --install-extension rebornix.ruby
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension samuelcolvin.jinjahtml
+code --install-extension teabyii.ayu
+code --install-extension tomoki1207.pdf
+code --install-extension vector-of-bool.cmake-tools
+code --install-extension zhuangtongfa.Material-theme
+code --install-extension zxh404.vscode-proto3
